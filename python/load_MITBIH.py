297914d6f51006ceeebd4081325e0c6f3fe00072
zhoujun <dcszhju@nus.edu.sg>
2019-07-08 14:16:18 +0800

    update with oversampling

--- python/load_MITBIH.py
+++ python/load_MITBIH.py
@@ -101,10 +101,14 @@ def load_mit_db(DS, winL, winR, do_preprocess, maxRR, use_RR, norm_RR, compute_m
 
     else:
         print("Loading MIT BIH arr (" + DS + ") ...")
+        DS11 = [101, 106, 108, 109, 114, 115, 116, 119, 122, 209, 223,
+               230]
+        DS12 = [112, 118, 124, 201, 203, 205, 207, 208, 215, 220, 230]
 
         # ML-II
         if reduced_DS == False:
-            DS1 = [101, 106, 108, 109, 112, 114, 115, 116, 118, 119, 122, 124, 201, 203, 205, 207, 208, 209, 215, 220, 223, 230]
+            DS1=DS11
+            #DS1 = [101, 106, 108, 109, 112, 114, 115, 116, 118, 119, 122, 124, 201, 203, 205, 207, 208, 209, 215, 220, 223, 230]
             DS2 = [100, 103, 105, 111, 113, 117, 121, 123, 200, 202, 210, 212, 213, 214, 219, 221, 222, 228, 231, 232, 233, 234]
 
         # ML-II + V1
@@ -131,6 +135,7 @@ def load_mit_db(DS, winL, winR, do_preprocess, maxRR, use_RR, norm_RR, compute_m
             gc.enable()
             f.close()
         else: # Load data and compute de RR features 
+
             if DS == 'DS1':
                 my_db = load_signal(DS1, winL, winR, do_preprocess)
             else:
@@ -460,7 +465,7 @@ def load_signal(DS, winL, winR, do_preprocess):
 
     size_RR_max = 20
 
-    pathDB = '/home/zhoujun/workspace/research/'
+    pathDB = '../'
     DB_name = 'mitdb'
     fs = 360
     jump_lines = 1
--- python/try.py
+++ python/try.py
@@ -5,12 +5,21 @@ def main(multi_mode='ovo', winL=90, winR=90, do_preprocess=True, use_weight_clas
          do_cross_val='', C_value=0.001, gamma_value=0.0, reduced_DS=False, leads_flag=[1, 0]):
     print("Runing train_SVM.py!")
 
-    db_path = '/home/zhoujun/workspace/research/mitdb/m_learning/'
+    db_path = '../mitdb/m_learning/'
 
     # Load train data
     [tr_features, tr_labels, tr_patient_num_beats] = load_mit_db('DS1', winL, winR, do_preprocess,
                                                                  maxRR, use_RR, norm_RR, compute_morph, db_path,
                                                                  reduced_DS, leads_flag)
+    # Filename
+    oversamp_features_pickle_name = create_oversamp_name(reduced_DS, do_preprocess, compute_morph, winL, winR, maxRR,
+                                                         use_RR, norm_RR, pca_k)
+
+    # Do oversampling
+    oversamp_method = 'SMOTE_regular'
+    tr_features, tr_labels = perform_oversampling(oversamp_method, db_path + 'oversamp/python_mit',
+                                                  oversamp_features_pickle_name, tr_features, tr_labels)
+
 
     # Load Test data
     [eval_features, eval_labels, eval_patient_num_beats] = load_mit_db('DS2', winL, winR, do_preprocess,
@@ -20,7 +29,14 @@ def main(multi_mode='ovo', winL=90, winR=90, do_preprocess=True, use_weight_clas
         np.savetxt('mit_db/' + 'exp_2_' + 'DS2_labels.csv', eval_labels.astype(int), '%.0f')
     else:
         np.savetxt('mit_db/' + 'DS2_labels.csv', eval_labels.astype(int), '%.0f')
+    
+    indices = np.arange(tr_labels.shape[0])
+    np.random.shuffle(indices)
+    x_train = tr_features[indices].astype(np.float32)
+    y_train = tr_labels[indices].astype(np.int64)
+    np.savez('paper180.npz', x_train=x_train, y_train=y_train, x_test=eval_features.astype(np.float32), y_test=eval_labels.astype(np.int64))
+
 
 
 if __name__ == '__main__':
-    main(reduced_DS=True, maxRR=False, use_RR=False, norm_RR=False, compute_morph=['raw'])
+    main(reduced_DS=False, maxRR=False, use_RR=False, norm_RR=False, compute_morph=['raw'])
